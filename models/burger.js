b72326451d4a425e282b08c62cfc0b109d68bd93
Jesse Emerson <jesseemerson7@gmail.com>
2023-05-09 10:07:58 -0400

    user model up to date

---
+++ models/burger.js
@@ -0,0 +1,30 @@
+const {Model,DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Burger extends Model{}
+
+Burger.init(
+    {
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        }
+    },
+    {
+        description:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                max:50
+            }
+        }
+    },
+    {
+        image:{
+            type:DataTypes.STRING,
+            defaultValue:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuyfkEcpJg3G6mokY4k5SzOXBGgDA_M4L1bw&usqp=CAU" 
+        }
+    }
+
+)
