8c3dd43baf0dd6b87754052bc15d4c949d9beb35
Jesse Emerson <jesseemerson7@gmail.com>
2023-05-09 13:10:29 -0400

    added burgeringredient modle to link burgers and ingredients together added to seed

--- models/index.js
+++ models/index.js
@@ -2,18 +2,19 @@ const  Burger = require('./burger')
 const Favorite = require('./favorite')
 const Ingredient = require('./ingredient')
 const User = require('./user')
+const BurgerIngredient = require('./burgerIngredient')
 
 // User-Burger association
 User.hasMany(Burger);
 Burger.belongsTo(User);
 
 // Burger-Ingredient association
-Burger.belongsToMany(Ingredient, { through: 'BurgerIngredients' });
-Ingredient.belongsToMany(Burger, { through: 'BurgerIngredients' });
+Burger.belongsToMany(Ingredient, { through: BurgerIngredient });
+Ingredient.belongsToMany(Burger, { through: BurgerIngredient });
 
 // Favorite association
 Favorite.belongsTo(User);
 Favorite.belongsTo(Burger);
 
 
-module.exports = {User,Burger,Ingredient,Favorite}
+module.exports = {User,Burger,Ingredient,Favorite, BurgerIngredient}
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,11 @@
 const sequelize = require("../config/connection");
-const { User, Burger, Ingredient, Favorite } = require("../models");
+const { User, Burger, Ingredient, Favorite, BurgerIngredient } = require("../models");
 
 const userData = require("./userData");
 const burgerData = require("./burgerData");
 const favoriteData = require("./favoriteData");
 const ingredientData = require('./ingredientData')
-
+const burgerIngredientData = require('./burgerIngredientData')
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
   await User.bulkCreate(userData, {
@@ -15,6 +15,7 @@ const seedDatabase = async () => {
   await Burger.bulkCreate(burgerData);
   await Ingredient.bulkCreate(ingredientData);
   await Favorite.bulkCreate(favoriteData);
+  await BurgerIngredient.bulkCreate(burgerIngredientData)
 
   console.log("All models were synched successfully");
 
