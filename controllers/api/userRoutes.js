bea26d4777c50f6047575b7a98ffe56ca7f25834
Alan Pereyro <alanpereyro0@gmail.com>
2023-05-11 00:02:00 -0400

    My burger

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -29,12 +29,6 @@ router.post("/", async (req,res) => {
     router.post('/login', async (req, res) => {
       try {
         const userData = await User.findOne({ where: { email: req.body.email } });
-        console.log("USR: ", userData.id)
-        console.log("USR: ", userData.dataValues.id)
-        req.session.save(() => {
-          req.session.user_id = userData.id;
-          req.session.logged_in = true;
-        });
 
         if (!userData) {
           res
--- public/js/post.js
+++ public/js/post.js
@@ -1,25 +1,31 @@
 const burgerFormHandeler = async (event) => {
   event.preventDefault();
-  console.log("hello");
+  console.log("Inside burgerFormHandeler");
+
   const name = document.getElementById("name").value.trim();
   const description = document.getElementById("description").value.trim();
   const ingredients = document.getElementById("ingredients").value.trim();
   console.log(name, description, ingredients);
-  const recipe = {
-    name: name,
-    description: description,
-    ingredients: ingredients,
-  };
-  const response = await fetch("/api/recipe/", {
-    method: "POST",
-    body: JSON.stringify(recipe),
-    headers: { "Content-Type": "application/json" },
-  });
-  if (response.ok) {
-    console.log(response);
-    // document.location.replace('/');
-  } else {
-    alert(response.statusText);
+
+
+  try {
+    window.location.href = "/mine";
+    const response = await fetch("/api/recipe/", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        description: description,
+        ingredients: ingredients,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    console.log("END OF FUN()");
+
+  } catch (error) {
+    console.log("Error", error);
   }
 };
 
